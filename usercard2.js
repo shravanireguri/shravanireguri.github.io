54959ab7ed595c2c6d439ace4d941cf884246cf3
shravanireguri <134955685+shravanireguri@users.noreply.github.com>
2023-06-06 13:41:25 +0530

    usercard2

--- usercard2.js
+++ usercard2.js
@@ -2,13 +2,13 @@ let dhonioldinfo = {
     imgurl: "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_320,q_50/lsci/db/PICTURES/CMS/319900/319946.png", 
     name: "Dhoni",
     Description: "Captain of Indian team"
-}
+};
 
 let dhoniyounginfo = {
     imgurl: "https://stat4.bollywoodhungama.in/wp-content/uploads/2016/10/M.S.-Dhoni-%E2%80%93-The-Untold-Story-7.jpg",
     name: "MS Dhoni",
     Description: "Ticket Collector"
-}
+};
 
 let isdhoniold = true;
 let displayObj;
